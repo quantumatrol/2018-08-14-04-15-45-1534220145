8cb8aafaddbbeaf03ada611e179dee29be8a4f4e
Wang Huancheng <wangkang980930@outlook.com>
2019-05-22 16:23:45 +0800

    finished reminder

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 // Write your cade below:
+<<<<<<< HEAD
 module.exports = function main() {
   
+=======
+module.exports = function main(dividend, divisor)
+{
+    return dividend%divisor;
+>>>>>>> finished reminder
 };
